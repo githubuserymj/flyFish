e28cc164f5e030cb7205af10bea118b8f1c02110
yumingjun <ymjgitpwd666>
2022-01-12 11:09:11 +0800

    commit code

---
+++ flyFishJob/src/main/java/com/flyfish/flyfishjob/FlyFishJobApplication.java
@@ -0,0 +1,13 @@
+package com.flyfish.flyfishjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyFishJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyFishJobApplication.class, args);
+    }
+
+}
---
+++ flyFishJob/src/test/java/com/flyfish/flyfishjob/FlyFishJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyfish.flyfishjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyFishJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
